9a9870063a9f9e54543b80793c25232c5370da04
PETR MIKHAILOV <122748858+Axe-On-You@users.noreply.github.com>
2024-12-12 20:57:52 +0300

    Add files via upload

---
+++ Матан/Matan_Lab1/Matan_Lab1/function.py
@@ -0,0 +1,6 @@
+﻿from numpy import arccos  # Используем библиотеку math для импорта функции arccos
+
+
+def func(x):
+    y = arccos(x**2) - x  # Исходная функция, для которой ищем корень
+    return y
---
+++ Матан/Matan_Lab1/Matan_Lab1/main.py
@@ -0,0 +1,15 @@
+﻿from BISECT import bisect
+from function import func
+
+
+# Задаем параметры для метода бисекций
+a = 0.5
+b = 1.0
+eps = 5 * 10**(-16)
+
+# Находим корень функции с помощью метода бисекций
+root, k = bisect(a, b, eps, func)
+
+# Выводим результат
+print("Корень функции:", root)
+print("Произошло итераций:", k)
